1c058f06806c31175503ba281042e186e5b39052
biki321 <39851862+biki321@users.noreply.github.com>
2021-09-28 22:53:14 +0530

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -53,7 +53,8 @@ import { ReqAcceptedNotif } from './notification/reqAcceptedNotif.entity';
           ReqGotNotif,
           ReqAcceptedNotif,
         ],
-        synchronize: false, //this should be false in production
+        synchronize: true, //this should be false in production
+        ssl: true,
       }),
       inject: [ConfigService],
     }),
